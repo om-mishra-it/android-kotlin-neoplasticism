766b53bd24e5edc0ec03de120a53343547003ca9
Om <ommishra32868@gmail.com>
2020-05-27 15:17:23 +0530

    Added Listeners for Red,Yellow,Green buttons

--- app/src/main/java/com/velocityx/neoplasticism/MainActivity.kt
+++ app/src/main/java/com/velocityx/neoplasticism/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -23,6 +24,11 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_four_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_five_text -> view.setBackgroundColor(Color.MAGENTA)
+
+            // Boxes using custom colors for background
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
@@ -33,9 +39,13 @@ class MainActivity : AppCompatActivity() {
         val boxFourText = findViewById<TextView>(R.id.box_four_text)
         val boxFiveText = findViewById<TextView>(R.id.box_five_text)
 
+        val yellowButton = findViewById<TextView>(R.id.yellow_button)
+        val greenButton = findViewById<TextView>(R.id.green_button)
+        val redButton = findViewById<TextView>(R.id.red_button)
+
         val clickableViews: List<View> =
             listOf(boxOneText, boxTwoText, boxThreeText,
-                boxFourText, boxFiveText, rootConstraintLayout)
+                boxFourText, boxFiveText, rootConstraintLayout,red_button,green_button,yellow_button)
 
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
